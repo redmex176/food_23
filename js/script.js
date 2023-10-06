518c6feb88faa21e3100b2c71223417d0c7bdaf5
Alexey <xomutof.alex176@gmail.com>
2023-10-06 12:38:14 +0300

    update calc

--- js/script.js
+++ js/script.js
@@ -407,9 +407,38 @@ if(slides.length < 10) {
 
 const result = document.querySelector('.calculating__result span');
 
-let sex = 'female', 
-    heght, weight, age, 
+let sex, heght, weight, age, ratio;
+
+if(localStorage.getItem('sex')) {
+    sex = localStorage.getItem('sex');
+} else { 
+    sex = 'female';
+    localStorage.setItem('sex', 'female');
+}
+
+if(localStorage.getItem('ratio')) {
+    ratio = localStorage.getItem('ratio');
+} else { 
     ratio = 1.375;
+    localStorage.setItem('ratio', 1.375);
+}
+
+function initLocalSettings(selector, activeClass) {
+    const elements = document.querySelectorAll(selector);
+
+    elements.forEach(elem => {
+        if(elem.getAttribute('id') === localStorage.getItem('sex')) {
+            elem.classList.add(activeClass);
+        }
+
+        if(elem.getAttribute('data-ratio') === localStorage.getItem('ratio')) {
+            elem.classList.add(activeClass);
+        }
+    });
+}
+
+initLocalSettings('#gender div', 'calculating__choose-item_active');
+initLocalSettings('.calculating__choose_big div', 'calculating__choose-item_active');
 
 function calcTotal() {
     if(!sex || !heght || !weight || !age || !ratio) {
@@ -433,8 +462,10 @@ function getStatInfo(parentSelector, activeClass) {
         elem.addEventListener('click', (e) => {
             if(e.target.getAttribute('data-ratio')) {
                 ratio = e.target.getAttribute('data-ratio');
+                localStorage.setItem('ratio', e.target.getAttribute('data-ratio'));
             } else { 
                 sex = e.target.getAttribute('id');
+                localStorage.setItem('sex', e.target.getAttribute('id'));
             }
     
             elements.forEach((elem) => {
@@ -455,6 +486,14 @@ function getDynamicInfo(selector) {
     const input = document.querySelector(selector);
 
     input.addEventListener('input', () => {
+
+        if(input.value.match(/\D/g)) {
+            input.style.boxShadow = '0 4px 15px red';
+            console.log('qwe');
+        } else {
+            input.style.boxShadow = '0 4px 15px rgba(0,0,0,.2)';
+        }
+
         switch(input.getAttribute('id')) {
             case 'height' : 
                 heght = +input.value;
